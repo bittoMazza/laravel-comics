4ec146387e8317070b51f54e59a6a57a198a17c2
MyUsername <robertomazza99@gmail.com>
2022-09-07 14:31:47 +0200

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="d-flex justify-content-between align-items-center nav-container py-4 container">
+      <img class="header-logo" src="../assets/img/dc-logo.png" alt="Logo dc">
+      <nav class="header-nav">
+        <ul>
+          <li>
+          ASDASDASD
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
